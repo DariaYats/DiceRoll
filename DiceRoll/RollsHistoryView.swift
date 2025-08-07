1a20d6a5b7f86b6a85f683d37b45fe70d5e92d60
Daria Yats <dariayatsyniuk@gmail.com>
2025-08-07 13:01:44 +0200

    add alert for delete all button

--- DiceRoll/RollsHistoryView.swift
+++ DiceRoll/RollsHistoryView.swift
@@ -11,20 +11,37 @@ struct RollsHistoryView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var rollHistory: [DiceRoll]
 
+    @State private var isShowingAlert = false
+
     var body: some View {
-        List {
-            Section(header: Text("Roll History")) {
-                ForEach(rollHistory.sorted(by: { $0.timestamp > $1.timestamp })) { roll in
-                    VStack(alignment: .leading) {
-                        Text("Total: \(roll.total)")
-                        Text("Rolls: \(roll.individualRolls.map { String($0) }.joined(separator: ", "))")
-                        Text("Dice: \(roll.diceAmount), Sides: \(roll.diceSides)")
-                        Text("Time: \(roll.timestamp, formatter: dateFormatter)")
-                            .font(.caption)
-                            .foregroundColor(.gray)
+        if rollHistory.isEmpty {
+            Text("Roll your first dice and check the history below!")
+        } else {
+            List {
+                Section(header: Text("Roll History")) {
+                    ForEach(rollHistory.sorted(by: { $0.timestamp > $1.timestamp })) { roll in
+                        VStack(alignment: .leading) {
+                            Text("Total: \(roll.total)")
+                            Text("Rolls: \(roll.individualRolls.map { String($0) }.joined(separator: ", "))")
+                            Text("Dice: \(roll.diceAmount), Sides: \(roll.diceSides)")
+                            Text("Time: \(roll.timestamp, formatter: dateFormatter)")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
                     }
+                    .onDelete(perform: deleteRoll)
                 }
-                .onDelete(perform: deleteRoll)
+            }
+            .toolbar {
+                Button("Delete all history", systemImage:  "trash") {
+                    isShowingAlert = true
+                }
+            }
+            .alert("Are you sure you want to delete all roll history? This action cannot be undone.", isPresented: $isShowingAlert) {
+                Button("Delete all") {
+                    deleteAll()
+                }
+                Button("Cancel", role: .cancel) { }
             }
         }
     }
